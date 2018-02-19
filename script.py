2e13cb9c1c110151e567e3a95222b116279ee0c4
Andy <amillion3@gmail.com>
2018-02-18 20:04:49 -0600

    added CNN as a source

--- script.py
+++ script.py
@@ -1,1 +1,23 @@
-import
+import newspaper
+
+cnn_paper = newspaper.build('http://cnn.com')
+
+for article in cnn_paper.articles:
+  print(article.url)
+
+for category in cnn_paper.category_urls():
+    print(category)
+
+article = cnn_paper.articles[0]
+
+article.download()
+
+article.html
+
+article.parse()
+
+article.authors
+
+article.text
+
+article.top_image
